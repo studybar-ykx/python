1ab0f347451134a2f90eb9d9915813a1ccb0f063
studybar-ykx <986237673@qq.com>
2020-12-21 21:41:45 +0800

    python learning note2

---
+++ float.py
@@ -0,0 +1,17 @@
+import sys
+
+#print(sys.float_info)
+
+print(int (4.5), float(4), complex(4))
+#complex is fushu
+type() #judge type
+x//y x**y = pow(x,y) abs()
+
+upper
+lower
+capitalalize
+strip
+split
+isdigit
+find
+replace
