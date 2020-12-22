b32d5477f2bcc6a56dfd5e4507ade181a0cf8f24
studybar-ykx <986237673@qq.com>
2020-12-22 21:07:45 +0800

    python learning note3

--- float.py
+++ float.py
@@ -4,8 +4,8 @@ import sys
 
 print(int (4.5), float(4), complex(4))
 #complex is fushu
-type() #judge type
-x//y x**y = pow(x,y) abs()
+#type() #judge type
+'''x//y x**y = pow(x,y) abs()
 
 upper
 lower
@@ -15,3 +15,9 @@ split
 isdigit
 find
 replace
+'''
+vlist = [0, 1, 2, 3, 4]
+for i in vlist[:3]:
+    print(i)
+print(vlist*2,len(vlist[2:]) )
+print(2 in vlist)
